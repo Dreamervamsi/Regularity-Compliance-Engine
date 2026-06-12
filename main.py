aa8ccdd05d5f585a0adbb208d2d9138b8e20d058
vamsi krishna <kvamsi.nellore@gmail.com>
2026-06-12 16:34:00 +0530

    Update api route to accept multiple files and convert file to streams

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
+from ingest_docs.ingest_manager import ingest
+import io
 
 app = FastAPI()
 
 @app.post('/ingest-doc')
-def main(file: UploadFile = File(...)):
-    print(file)
+def main(files:list[UploadFile] = File(...)):
+    file_info=[]
+    for file in files:
+        binary_content = file.file.read()
+        io_bytes = io.ByteIO(binary_content)
+
+        file_info.append({
+            "stream":io_bytes,
+            "file_name":file.filename
+        })
+
+    ingest(file_info)
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app", 
-        host="127.0.0.1", 
-        port=8000, 
-        reload=True, 
-        reload_excludes=[".venv/*"]
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True
     )
