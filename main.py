5514b04ee879e0743275852219598922fc536a03
vamsi krishna <kvamsi.nellore@gmail.com>
2026-06-13 18:56:45 +0530

    add route for query intake

--- main.py
+++ main.py
@@ -6,18 +6,31 @@ import io
 app = FastAPI()
 
 @app.post('/ingest-doc')
-def main(files:list[UploadFile] = File(...)):
-    file_info=[]
-    for file in files:
-        binary_content = file.file.read()
-        io_bytes = io.ByteIO(binary_content)
+def ingest(files:list[UploadFile] = File(...)):
+    try :
+        file_info=[]
+        for file in files:
+            binary_content = file.file.read()
+            io_bytes = io.ByteIO(binary_content)
 
-        file_info.append({
-            "stream":io_bytes,
-            "file_name":file.filename
-        })
+            file_info.append({
+                "stream":io_bytes,
+                "file_name":file.filename
+            })
 
-    ingest(file_info)
+        # ingesting document
+        ingest(file_info)
+        return "Ingestion successfully completed!"
+    except Exception as e:
+        print(f'Error :{e}')
+        return "Failed to ingest documents!"
+
+@app.post('/query')
+def query(query:str):
+    
+    # query preprocessing
+    preprocess_query(query)
+
 
 if __name__ == "__main__":
     uvicorn.run(
