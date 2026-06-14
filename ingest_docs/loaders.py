aaec6e4ca42b83c7a6363401e265171d9f5aa323
vamsi krishna <kvamsi.nellore@gmail.com>
2026-06-14 15:35:25 +0530

    update metadata to include page number

--- ingest_docs/loaders.py
+++ ingest_docs/loaders.py
@@ -4,18 +4,22 @@ from pypdf import PdfReader
 
 def file_loader(file_info:list)->list:
     results=[]
+    pattern = r'(?m)^((?:Section|Article|Part|Chapter)\s+[\dIVXLC]+(?:\.\d+)*[.:]?\s*.+)$'
+
     for file in file_info:   
         stream = file['stream']
         file_name = file['file_name']
-        extracted_text=""
+        file_no = file['file_no']
         doc=PdfReader(stream)
-        for page in doc.pages:
-            text=page.extract_text()
-            if text:
-                extracted_text+=text + "\n"
 
-        pattern = r'(?m)^((?:Section|Article|Part|Chapter)\s+[\dIVXLC]+(?:\.\d+)*[.:]?\s*.+)$'
+        page_content=[]
+        for page_num,page in enumerate(doc.pages,start=1):
+            text=page.extract_text()
+            if text:    
+                page_content.append({"page_num":page_num,"text":text})
         
+        extracted_text = "\n".join(page_content)
+
         parts = re.split(pattern,extracted_text, flags=re.MULTILINE)
 
         for i in range(1,len(parts),2):
@@ -28,7 +32,8 @@ def file_loader(file_info:list)->list:
                     "id":str(uuid.uuid4()),
                     "section_title":section_title,
                     "text":content,
-                    "source_path":file_name
+                    "source_path":file_name,
+                    "file_no":file_no
                 })
     
     return results
