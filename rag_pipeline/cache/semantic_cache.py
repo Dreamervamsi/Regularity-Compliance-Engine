190843befbd2e69b5348aad32f7656e41adb45a9
vamsi krishna <kvamsi.nellore@gmail.com>
2026-06-16 11:48:09 +0530

    update code for implementing semantic cache

--- rag_pipeline/cache/semantic_cache.py
+++ rag_pipeline/cache/semantic_cache.py
@@ -1,16 +1,21 @@
+import config
+import asyncio
 import hashlib
+from fastapi import Depends
+from redis.commands.search.field import TextField, VectorField
+from redis.commands.search.query import Query
 from redis.asyncio import Redis, ConnectionPool
+import numpy as np
 
 # redis is single threaded.
 # redis has logical databases 0-15 so using db=0
 # is best way rather than relying on other dbs.
 
 redis_pool:ConnectionPool = ConnectionPool(
-    host="localhost",
-    port=6379,
-    db=0,
-    decode_responses=True # by setting it True, it will automatically 
-                          #converts redis bytes to strings
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT,
+    db=config.REDIS_DB,
+    decode_responses=False
 )
 
 async def get_redis():
@@ -22,22 +27,24 @@ async def get_redis():
     finally:
         await client.close()
 
+""" Searching for exact query (1st phase of dual redis search) """
+
 def get_query_hash(query:str):
     return hashlib.sha256(query.encode('utf-8')).hexdigest()
 
-async def search_query(query:str,redis:Redis = Depends(get_redis)):
+async def search_exact_query(query:str,redis:Redis = Depends(get_redis)):
     try:
         query_hash = get_query_hash(query)
         cache_data = await redis.get(query_hash)
         if cache_data:
-            return cache_data
-        return "Query missing!"
+            return cache_data.decode('utf-8') if isinstance(cache_data, bytes) else cache_data
+        return None
 
     except Exception as e:
         print(e)
         return None
 
-async def store_query(query:str,value:str,redis:Redis = Depends(get_redis)):
+async def store_exact_query(query:str,value:str,redis:Redis = Depends(get_redis)):
     try:
         query_hash = get_query_hash(query)
         await redis.set(query_hash,value)
@@ -46,14 +53,73 @@ async def store_query(query:str,value:str,redis:Redis = Depends(get_redis)):
         print(e)
         return None
 
-async def main():
-    async for client in get_redis():
-        await store_query("What is the capital of France?","Paris",client)
-        res1 = await search_query("What is the capital of France?",client)
-        res2 = await search_query("What is the capital of Germany?",client)
+""" Searching for similar query (2nd phase of dual redis search) """
 
-        print(res1)
-        print(res2)
+async def _init_index(redis:Redis = Depends(get_redis)):
+    try:
+        schema = (
+            TextField("query"),
+            TextField("response"),
+            VectorField(
+                "embedding",
+                "HNSW",
+                {
+                    "TYPE":"FLOAT32",
+                    "DIM":config.SEMANTIC_CACHE_DIM,
+                    "DISTANCE_METRIC":"COSINE"
+                }
+            )
+        )
+        await redis.ft(config.REDIS_INDEX).create_index(fields=schema)
+        print("Index created!")
+    except:
+        raise Exception("Cannot create index")
 
-if __name__ == "__main__":
-    asyncio.run(main())
+async def store_semantic_query(query:str,query_embedding, response: str, redis: Redis=Depends(get_redis)):
+    try:
+        query_id = get_query_hash(query)
+        key = f"{config.REDIS_PREFIX}{query_id}"
+        await redis.hset(key, mapping={
+            "query":query,
+            "response":response
+        })
+        query_embedding_bytes = np.array(query_embedding, dtype=np.float32).tobytes()
+
+        await redis.hset(key,"embedding",query_embedding_bytes)
+        await redis.expire(key,time=60*60*24)
+        return "Query stored successfully!"
+    except Exception as e:
+        print(e)
+        return None
+
+async def search_semantic_query(query_embeddings, redis: Redis=Depends(get_redis), threshold: float = 0.25):
+    try:
+        query_embedding_bytes = np.array(query_embeddings, dtype=np.float32).tobytes()
+
+        search_query = (
+            Query("*=>[KNN 1 @embedding $query_vector AS vector_score]")
+            .return_fields("query", "response", "embedding")
+            .sort_by("vector_score")
+            .dialect(2)
+        )
+        result = await redis.ft(config.REDIS_INDEX).search(
+            search_query,
+            query_params={"query_vector": query_embeddings}
+        )
+
+        if result.docs:
+            matched_doc = result.docs[0]
+            # In COSINE distance, a smaller score means the vectors are closer together (0.0 = identical)
+            distance_score = float(matched_doc.vector_score)
+            if distance_score <= threshold:
+                return {
+                    "query": matched_doc.query.decode('utf-8') if isinstance(matched_doc.query, bytes) else matched_doc.query,
+                    "response": matched_doc.response.decode('utf-8') if isinstance(matched_doc.response, bytes) else matched_doc.response,
+                    "score": distance_score
+                }
+        return {"match_found": False, "message": "No semantically similar query found inside threshold."}
+
+    except Exception as e:
+        print(e)
+        return None
+ 
